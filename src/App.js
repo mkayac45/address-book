cab634b06f0b6a6cd9d37d660009dd0cc509f131
Mehmet KAYACI <mkayac45@gmail.com>
2022-06-01 20:57:24 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Contacts from './Components/contacts/Contacts';
 import { Navbar } from './Components/Navbar';
 import{provider ,Provider} from "react-redux";
 import store from "./store";
-import {Router,Route,Switch} from "react-router-dom";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import AddContact from './Components/contacts/AddContact';
 function App() {
   return (
+    
     <Provider store = {store}>
       <Router>
         <div className="App">
           <Navbar/>
           <div className='container'>
             <div className='py-3'>
-              <Switch>
-                <Route exact path="/" component={Contacts}/>
-                <Route exact path="/contacts/add" component={AddContact}/>
-              </Switch>
-
+              <Routes>
+                <Route  path="/" element={<Contacts/>}/>
+                <Route  path="/contacts/add" element={<AddContact/>}/>
+              </Routes>
             </div>
           </div>
       </div>
