4ed1d7f2116248fff47e9e101f84e70f66a8f5d9
Mehmet KAYACI <mkayac45@gmail.com>
2022-06-01 21:14:46 +0300

    initial commit

--- src/Components/contacts/AddContact.js
+++ src/Components/contacts/AddContact.js
@@ -3,7 +3,61 @@ import React from 'react'
 const AddContact = () => {
   return (
     <div>
-        <p>fdsdfasdfsdaf</p>
+        <div className='card border-0 shadow'>
+          <div className='card-header'>Add Contact</div>
+          <div className='card-body'>
+            <form>
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+
+              <div className='form-group'>
+                <input 
+                  type="text"
+                  className='form-control'
+                  placeholder='Enter Your Name' />
+              </div>
+            </form>
+          </div>
+        </div>
     </div>
   )
 }
